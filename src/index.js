83837a134eafb8195a9b62226d7617a98dc12ed8
KevinAdiwiguna <f1d02410115@student.unram.ac.id>
2024-10-10 20:57:18 +0800

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import fs from 'fs/promises';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { fileURLToPath, pathToFileURL } from 'url';
+import { dirname, join } from 'path';
+import fs from "fs";
 import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-dotenv.config();
 
 const client = new Client({
   intents: [
@@ -19,64 +19,28 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
 
-async function loadCommands() {
-  try {
-    const commandFolders = await fs.readdir(foldersPath);
+const functionFolders = fs.readdirSync(join(__dirname, "../src/functions"));
+for (const folder of functionFolders) {
+  const functionFiles = fs
+    .readdirSync(join(__dirname, `../src/functions/${folder}`))
+    .filter((file) => file.endsWith(".js"));
 
-    for (const folder of commandFolders) {
-      const commandsPath = path.join(foldersPath, folder);
-      const commandFiles = await fs.readdir(commandsPath);
-      for (const file of commandFiles) {
-        if (!file.endsWith('.js')) continue;
+  for (const file of functionFiles) {
+    const filePath = join(__dirname, `../src/functions/${folder}/${file}`);
+    const fileUrl = pathToFileURL(filePath).href;
 
-        const filePath = path.join(commandsPath, file);
-        const { default: command } = await import(new URL(`file://${filePath}`).pathname);
-
-        if ('data' in command && 'execute' in command) {
-          client.commands.set(command.data.name, command);
-        } else {
-          console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-      }
-    }
-  } catch (err) {
-    console.error('Error loading commands:', err);
+    import(fileUrl).then((module) => {
+      module.default(client);
+    }).catch((err) => {
+      console.error(`Error importing file ${filePath}:`, err);
+    });
   }
 }
 
-async function loadEvents() {
-  try {
-    const eventsPath = path.join(__dirname, 'events');
-    const eventFiles = await fs.readdir(eventsPath);
 
-    for (const file of eventFiles) {
-      if (!file.endsWith('.js')) continue;
-
-      const filePath = path.join(eventsPath, file);
-      const { default: event } = await import(new URL(`file://${filePath}`).pathname);
-
-      if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-      } else {
-        client.on(event.name, (...args) => event.execute(...args));
-      }
-    }
-  } catch (err) {
-    console.error('Error loading events:', err);
-  }
+try {
+  await client.login(process.env.token);
+} catch (err) {
+  console.error('Error logging in:', err);
 }
-
-async function startBot() {
-  await loadCommands();
-  await loadEvents();
-  
-  try {
-    await client.login(process.env.token);
-  } catch (err) {
-    console.error('Error logging in:', err);
-  }
-}
-
-startBot();
